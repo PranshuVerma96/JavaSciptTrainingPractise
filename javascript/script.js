9aaa9ea06004771310ec97ba78bbe9ac73f58b7f
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-01-31 13:24:05 +0530

    Add files via upload
    this is only practise file and for me

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+// alert('how are you js user');
+
+// this is variable assign 
+let myName = 'Pranshu';
+const age = 21;
+let isStudent = true;
+console.log(myName);
+
+// math matical calcuation 
+
+let data1 = 10;
+let data2 = 20;
+ 
+let result1 = data1 + data2;
+console.log('the addtion is : ', result1 );
+let result2 = data1 - data2;
+console.log('the sub is : ', result1);
+let result3 = data1 * data2;
+console.log('the mul is : ', result3 );
+let result4 = data1 / data2;
+console.log('the divisition  is : ', result4 );
