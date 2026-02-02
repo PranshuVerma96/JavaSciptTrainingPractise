00a18580a4912c885faa73437fb1ca179c011c2e
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-02-02 13:30:28 +0530

    Add files via upload
    this is my operator file and basic operator operation .

---
+++ jsDay2Operator.js
@@ -0,0 +1,61 @@
+// operator
+
+// or operator ||   real life example 
+
+let isStudent = true;
+let isSeniorCitizon = false;
+
+if(isStudent  || isSeniorCitizon ){
+  //console.log('You are abliavle for discount ');
+  
+}else{
+  //console.log("try other ways for discount");
+  
+}
+
+
+// not operator 
+
+let isLoggedIn = false
+if(!isLoggedIn){
+  console.log('Please loging first ');
+  
+}
+// assignmet  operator 
+
+//first example
+let value = 100;
+value += 50;
+console.log(value);
+
+
+//second example
+let blance = 200;
+blance -= 80;
+console.log(blance);
+
+
+//third example
+let marks = 100
+marks *= 5
+console.log(marks);
+
+
+//forth example
+let num = 2
+num **=3
+console.log(num);
+
+
+//five example
+let div = 100
+div %= 99
+console.log(div);
+
+
+
+
+
+
+
+
